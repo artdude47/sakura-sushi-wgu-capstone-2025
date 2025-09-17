b25b6c19dc07e997b65fe4593652ac181ded1fe2
artdude47 <artcouser@gmail.com>
2025-09-17 16:27:06 -0400

    Setup project

---
+++ src/SakuraSushi/SakuraSushi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SakuraSushi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
